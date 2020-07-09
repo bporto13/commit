0237c6a71ea7a14132a586e9d71f74dbba60a0b9
Shaumik <bportocarrero18@gmail.com>
2020-07-09 15:19:41 +0000

    first commit

---
+++ cipher.py
@@ -0,0 +1,12 @@
+# Code out your function definitions here
+
+#encode function
+#take in 2 arguments
+#first will be what we want to code
+#second how much we are shifting
+
+def endcode (sentence,num):
+    #loop through the string looking at each of the letters
+
+
+#decode function
